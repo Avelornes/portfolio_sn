8ef6c8ef6a615ae1f05e3b42820b6121c99d8a19
Avelornes <severine.noel94@gmail.com>
2017-04-06 09:54:42 +0200

    corrections finales

--- ressources/js/custom.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Séverine Noël on 17/01/17.
- */
-$(document).ready(function () {
-    $(".button-collapse").sideNav();
-    $('.parallax').parallax();
-    $('.modal').modal();
-})
